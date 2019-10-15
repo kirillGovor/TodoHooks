8af25648e48afdd9095bf04bc7c1f26668f3617c
Kirill Govor <kirillll13562@gmail.com>
2019-10-15 11:21:31 +0300

    Add todo

---
+++ react-to-do/src/App.js
@@ -0,0 +1,64 @@
+import './App.css';
+import React, { useState } from 'react';
+import { Todo } from './components/Todo';
+import { TodoForm } from './components/TodoForm';
+
+function App() {
+  const addTodo = text => {
+    const newTodos = [...todos, { text, isCompleted: false }];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted === true ? newTodos[index].isCompleted = false : newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  const [todos, setTodos] = useState([
+    {
+      text: "Learn about React",
+      isCompleted: false
+    },
+    {
+      text: "Meet friend for lunch",
+      isCompleted: false
+    },
+    {
+      text: "Build really cool todo app",
+      isCompleted: false
+    },
+    {
+      text: "finished it",
+      isCompleted: false
+    }
+  ]);
+
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+            completeTodo={completeTodo}
+            removeTodo={removeTodo}
+          />
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
+
+    </div>
+  );
+
+}
+
+
+export default App;
